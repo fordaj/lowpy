Fixed hidden layer dedz calculation

mempyml/mlp.py
@@ -3,103 +3,195 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 import numpy as np
 import time
+from keras.datasets import mnist
+import numpy as np
+import math
 
-
+# Start timer from 0
 def tic():
     global t
     t = time.time()
 
+# Lap timer
 def toc():
     global t
     print(time.time() - t)
 
+#Copy variables from host to GPU
+def hostToDevice(host_variable):
+    device_variable = cuda.mem_alloc(host_variable.nbytes)
+    cuda.memcpy_htod(device_variable, host_variable)
+    return device_variable
+
+
+
+###############################
+#----- MODEL ARCHITECTURE-----#
+###############################
+
 class Sequential:
+    # Constructor for model initialization
     def __init__(self):
         self.layer = []
+        self.numLayers = 0
+    
+    # Function for appending layer objects to the model
     def add(self,layer_type):
         self.layer.append(layer_type)
         numLayers = len(self.layer)
         if (numLayers == 1):
-            self.layer[0].build(self.layer[0].I,self.layer[0].J)
+            self.layer[0].build(self.layer[0].I_h,self.layer[0].J_h)
         else:
-            self.layer[numLayers-1].build(self.layer[numLayers-2].J,self.layer[numLayers-1].J)
+            self.layer[numLayers-1].build(self.layer[numLayers-2].J_h,self.layer[numLayers-1].J_h)
             self.layer[numLayers-1].linkPreviousLayer(self.layer[numLayers-2])
             self.layer[numLayers-2].linkNextLayer(self.layer[numLayers-1])
+        self.numLayers += 1
+    
+    # Copy all layer attributes from GPU to host
     def deviceToHost(self):
         for l in self.layer:
             l.deviceToHost()
-    def propagate(self):
+
+    # Forward pass
+    def propagate(self, input):
+        self.layer[0].x_d = input
         for l in self.layer:
             l.propagate()
-    def backpropagate(self):
-        numLayers = len(self.layer)
-        self.layer[numLayers-1].backpropagate(np.float32(5))
-        for l in range(numLayers-2,-1,-1):
+            
+    # Backward pass
+    def backpropagate(self,label):
+        self.layer[self.numLayers-1].backpropagate(label)
+        for l in range(self.numLayers-2,-1,-1):
             self.layer[l].backpropagate()
 
+    # Decision
+    def inference(self, label):
+        self.layer[self.numLayers-1].argmax(label)
+
+    # Test model
+    def validate(self, testData_d, testLabels_d):
+        numTests = len(testData_d)
+        hits = 0
+        self.layer[self.numLayers-1].hits_h = 0
+        for i in range(numTests):
+            self.propagate(testData_d[i])
+
+    # Train model
+    def fit(self, trainData, trainLabels, epochs, batch_size, validation_data):
+        numTrain = len(trainData)
+        # Dataset to device
+        print("Sending dataset to GPU...")
+        trainData_d     = []
+        trainLabels_d   = []
+        testData_d      = []
+        testLabels_d    = []
+        for i in range(numTrain):
+            trainData_d.append(hostToDevice(trainData[i]))
+        trainLabels_d = np.int32(trainLabels)
+        for i in range(500):#len(testData)):
+            testData_d.append(hostToDevice(testData[i]))
+        testLabels_d = np.int32(testLabels)
+        # Train network
+        tic()
+        hits = []
+        print("Training...")
+        for epoch in range(epochs):
+            print("Epoch " + str(epoch))
+            self.layer[self.numLayers-1].resetHits()
+            for i in range(numTrain):
+                #self.deviceToHost()
+                self.propagate(trainData_d[i])
+                #self.deviceToHost()
+                self.backpropagate(trainLabels_d[i])
+                #self.deviceToHost()
+                self.inference(trainLabels_d[i])
+            self.deviceToHost()
+            hits.append(self.layer[self.numLayers-1].hits_h[0])
+            print(hits)
+        toc()
+        return True
+
+
+
+#################################
+#-----FULLY CONNECTED LAYER-----#
+#################################
 
 class Dense:
+    # Constructor for model initialization
     def __init__(self, output_shape, input_shape=784, alpha=0.01):
-        self.I = input_shape
-        self.J = output_shape
-        self.alpha = np.float32(alpha)
+        self.I_h = input_shape
+        self.J_h = output_shape
+        self.alpha = np.float64(alpha)
+
+    # Layer constructor
     def build(self,input_shape,output_shape):
-        self.I = input_shape
-        self.J = output_shape
-        self.x = np.ones(self.I,dtype=np.float32)
-        self.x_d = cuda.mem_alloc(self.x.nbytes)
-        cuda.memcpy_htod(self.x_d, self.x)
-        self.w = np.random.rand(self.J,self.I).astype(np.float32) * 2 - 1
-        self.w_d = cuda.mem_alloc(self.w.nbytes)
-        cuda.memcpy_htod(self.w_d, self.w)
-        self.b = np.random.rand(self.J).astype(np.float32) * 2 - 1
-        self.b_d = cuda.mem_alloc(self.b.nbytes)
-        cuda.memcpy_htod(self.b_d, self.b)
-        self.y = np.zeros(self.J,dtype=np.float32)
-        self.y_d = cuda.mem_alloc(self.y.nbytes)
-        cuda.memcpy_htod(self.y_d, self.y)
-        self.z = np.zeros(self.J,dtype=np.float32)
-        self.z_d = cuda.mem_alloc(self.z.nbytes)
-        cuda.memcpy_htod(self.z_d, self.z)
-        self.dedz = np.zeros(self.J,dtype=np.float32)
-        self.dedz_d = cuda.mem_alloc(self.dedz.nbytes)
-        cuda.memcpy_htod(self.dedz_d, self.dedz)
-        self.dzdy = np.zeros(self.J,dtype=np.float32)
-        self.dzdy_d = cuda.mem_alloc(self.dzdy.nbytes)
-        cuda.memcpy_htod(self.dzdy_d, self.dzdy)
-        self.J_n = self.J
-        self.w_dn = self.w_d
-        self.z_dn = self.z_d
-        self.dedz_dn = self.dedz_d
-        self.dzdy_dn = self.dzdy_d
+        # Layer dimensions
+        self.I_h    = input_shape
+        self.I_d    = np.int32(self.I_h)
+        self.J_h    = output_shape
+        self.J_d    = np.int32(self.J_h)
+        # Layer parameters
+        self.x_h        = np.ones(self.I_h,dtype=np.float64)
+        self.x_d        = hostToDevice(self.x_h)
+        #self.w_h        = np.random.rand(self.J_h,self.I_h).astype(np.float64) * 2 - 1
+        #self.w_h        = np.ones((self.J_h,self.I_h),dtype=np.float64) * 0.01
+        self.w_h        = np.random.normal(0,math.sqrt(2/784),(self.J_h,self.I_h)).astype(np.float64)
+        self.w_d        = hostToDevice(self.w_h)
+        #self.b_h        = np.random.rand(self.J_h).astype(np.float64) * 2 - 1
+        #self.b_h        = np.ones(self.J_h,dtype=np.float64) * 0.01
+        self.b_h        = np.random.normal(0,math.sqrt(2/784),self.J_h).astype(np.float64)
+        self.b_d        = hostToDevice(self.b_h)
+        self.y_h        = np.zeros(self.J_h,dtype=np.float64)
+        self.y_d        = hostToDevice(self.y_h)
+        self.z_h        = np.zeros(self.J_h,dtype=np.float64)
+        self.z_d        = hostToDevice(self.z_h)
+        self.dedz_h     = np.zeros(self.J_h,dtype=np.float64)
+        self.dedz_d     = hostToDevice(self.dedz_h)
+        self.dzdy_h     = np.zeros(self.J_h,dtype=np.float64)
+        self.dzdy_d     = hostToDevice(self.dzdy_h)
+        self.n_J_d      = self.J_d
+        self.n_w_d      = self.w_d
+        self.n_z_d      = self.z_d
+        self.n_dedz_d   = self.dedz_d
+        self.n_dzdy_d   = self.dzdy_d
+        self.hits_h     = np.zeros(self.J_h,dtype=np.float64)
+        self.hits_d     = hostToDevice(self.hits_h)
+        # Cuda programs
         self.program = SourceModule("""
         __global__ void propagate(
             const int I,
-            const float *x,
-            const float *w,
-            const float *b,
-            float *y,
-            float *z
+            const double *x,
+            const double *w,
+            const double *b,
+            double *y,
+            double *z
             ){
                 int j = blockIdx.x;
-                float sum = 0;
+                int J = blockDim.x;
+                double sum = 0;
                 for (int i = 0; i < I; i++) sum += x[i]*w[j*I+i];
                 y[j] = sum + b[j];
                 z[j] = 1/(1 + exp(-y[j]));
             }
         __global__ void backpropagate(
-                float alpha,
-                float *dedz,
-                float *dzdy,
-                const float *z,
-                float *b,
-                float label = -1,
-                int J_n = -1,
-                const float *dedz_n = NULL,
-                const float *dzdy_n = NULL,
-                const float *w_n = NULL
+                const int label,
+                double *dedz,
+                const double *z,
+                const int J_n,
+                double *w_n,
+                const double *dedz_n,
+                const double *dzdy_n,
+                double *dzdy,
+                const double alpha,
+                const int I,
+                double *b,
+                double *w,
+                double *x
             ){
-                int j = blockIdx.x;
+                int j   = blockIdx.x;
+                int J   = blockDim.x;
+                int I_n = blockDim.x;
                 if (label > -1){
                     if (j == label){
                         dedz[j] = z[j] - 1;
@@ -107,43 +199,147 @@ class Dense:
                         dedz[j] = z[j] - 0;
                     }
                 }else{
-                    int J = blockDim.x;
-                    float sum = 0;
-                    for (unsigned long i = 0; i < J_n; i++) sum += w_n[i*J+j] * dedz_n[i] * dzdy_n[i];
+                    double sum = 0;
+                    for (int j_n = 0; j_n < J_n; j_n++) sum += w_n[j+j_n*I_n] * dedz_n[j_n] * dzdy_n[j_n];
                     dedz[j] = sum;
                 }
                 dzdy[j] = z[j] * (1 - z[j]);
-                b[j] -= alpha * dedz[j] * dzdy[j];
+                b[j]   -= alpha * dedz[j] * dzdy[j];
+                for (int i = 0; i < I; i++) w[j*I+i] -= alpha * dedz[j] * dzdy[j] * x[i];
+            }
+        __global__ void argmax(
+                const int label,
+                const double *z,
+                double *hits
+            ){
+                int j = blockIdx.x;
+                int J = blockDim.x;
+                if (j == 0){
+                    double maxVal = 0;
+                    int maxIdx = 0;
+                    for (int i = 0; i < 10; i++){
+                        if (z[i] > maxVal){
+                            maxIdx = i;
+                            maxVal = z[i];
+                        }
+                    }
+                    if (maxIdx == label){
+                        hits[0] += 1;
+                    }
+                    hits[1] = maxIdx;
+                    hits[2] = maxVal;
+                }
             }
         """)
+
+    # Link attributes from next layer into current layer
     def linkNextLayer(self, nextLayer):
-        self.J_n = nextLayer.J
-        self.w_dn = nextLayer.w
-        self.z_dn = nextLayer.z_d
-        self.dedz_dn = nextLayer.dedz_d
-        self.dzdy_dn = nextLayer.dzdy_d
+        self.n_J_d      = nextLayer.J_d
+        self.n_w_d      = nextLayer.w_d
+        self.n_z_d      = nextLayer.z_d
+        self.n_dedz_d   = nextLayer.dedz_d
+        self.n_dzdy_d   = nextLayer.dzdy_d
+
+    # Set inputs of current layer equal to outputs of previous layer
     def linkPreviousLayer(self, previousLayer):
-        self.x = previousLayer.z
-        self.x_d = previousLayer.z_d
+        self.x_d    = previousLayer.z_d
+
+    # Copy layer attributes from GPU back to host
     def deviceToHost(self):
-        cuda.memcpy_dtoh(self.x, self.x_d)
-        cuda.memcpy_dtoh(self.w, self.w_d)
-        cuda.memcpy_dtoh(self.b, self.b_d)
-        cuda.memcpy_dtoh(self.y, self.y_d)
-        cuda.memcpy_dtoh(self.z, self.z_d)
-        cuda.memcpy_dtoh(self.dedz, self.dedz_d)
-        cuda.memcpy_dtoh(self.dzdy, self.dzdy_d)
+        cuda.memcpy_dtoh(self.x_h, self.x_d)
+        cuda.memcpy_dtoh(self.w_h, self.w_d)
+        cuda.memcpy_dtoh(self.b_h, self.b_d)
+        cuda.memcpy_dtoh(self.y_h, self.y_d)
+        cuda.memcpy_dtoh(self.z_h, self.z_d)
+        cuda.memcpy_dtoh(self.dedz_h, self.dedz_d)
+        cuda.memcpy_dtoh(self.dzdy_h, self.dzdy_d)
+        cuda.memcpy_dtoh(self.hits_h, self.hits_d)
+
+    # Reset the hit counter
+    def resetHits(self):
+        self.hits_h = np.zeros(self.J_h,dtype=np.float64)
+        self.hits_d = hostToDevice(self.hits_h)
+
+    # Propagate 
     def propagate(self):
         propagate = self.program.get_function("propagate")
-        propagate(np.int32(self.I), self.x_d, self.w_d, self.b_d, self.y_d, self.z_d, block=(1,1,1), grid=(self.J,1,1))
-    def inference(self,label):
-        i=20
-    def backpropagate(self, label=np.float32(-1)):
+        propagate(
+                self.I_d, 
+                self.x_d, 
+                self.w_d, 
+                self.b_d, 
+                self.y_d, 
+                self.z_d,
+                block=(1,1,1), 
+                grid=(self.J_h,1,1)
+        )
+
+    # Backpropagate
+    def backpropagate(self, label=np.int32(-1)):
         backpropagate = self.program.get_function("backpropagate")
-        if label == np.float32(-1):
-            backpropagate(self.alpha, self.dedz_d, self.dzdy_d, self.z_d, self.b_d, label, block=(1,1,1), grid=(self.J,1,1))
-        else:
-            backpropagate(self.alpha, self.dedz_d, self.dzdy_d, self.z_d, self.b_d, label, np.int32(self.J_n), self.dedz_dn, self.dzdy_dn, self.w_dn, block=(1,1,1), grid=(self.J,1,1))
+        backpropagate(
+                label,
+                self.dedz_d,
+                self.z_d,
+                self.n_J_d,
+                self.n_w_d,
+                self.n_dedz_d,
+                self.n_dzdy_d,
+                self.dzdy_d,
+                self.alpha,
+                self.I_d,
+                self.b_d,
+                self.w_d,
+                self.x_d,
+                block=(1,1,1), 
+                grid=(self.J_h,1,1)
+        )
+
+    # Find winning neuron
+    def argmax(self, label):
+        argmax = self.program.get_function("argmax")
+        argmax(
+                label,
+                self.z_d,
+                self.hits_d,
+                block=(1,1,1), 
+                grid=(self.J_h,1,1)
+        )
+
+
+
+
+
+#-----IMPORT DATASET-----#
+(trainData, trainLabels), (testData, testLabels) = mnist.load_data()
+trainData = np.float64(trainData.reshape(trainData.shape[0],trainData.shape[1]*trainData.shape[2])) # Reshape train data from [n,28,28] to [n,784]
+trainLabels = np.int32(trainLabels)
+testData = np.float64(testData.reshape(testData.shape[0],testData.shape[1]*testData.shape[2])) # Reshape test data from [n,28,28] to [n,784]
+testLabels = np.int32(testLabels)
+
+#-----NORMALIZE DATA-----#
+trainData = np.true_divide(trainData, max(np.max(trainData), np.max(testData)))
+testData = np.true_divide(testData, max(np.max(trainData), np.max(testData)))
+
+#-----BUILD MODEL-----#
+model = Sequential()
+model.add(Dense(533,input_shape=784,alpha=0.2))
+model.add(Dense(533,alpha=0.2))
+model.add(Dense(10,alpha=0.2))
+
+#-----TRAIN MODEL-----#
+history = model.fit(trainData, trainLabels, epochs=10, batch_size=60000, validation_data=(testData, testLabels))
+print(history)
+		
+
+
+
+
+
+
+
+
+
 
 
 
@@ -152,16 +348,3 @@ class Dense:
 # model.add(Dense(350, input_shape=input_shape, activation='relu'))
 # model.add(Dense(50, activation='relu'))
 # model.add(Dense(num_classes, activation='softmax'))
-
-model = Sequential()
-model.add(Dense(533,input_shape=784))
-model.add(Dense(533))
-model.add(Dense(10))
-
-tic()
-model.deviceToHost()
-model.propagate()
-model.deviceToHost()
-model.backpropagate()
-model.deviceToHost()
-toc()