5be7da10e8ac5793798fdcc50116c1b6842792ff
fordaj <26288298+fordaj@users.noreply.github.com>
2021-01-28 05:14:44 -0500

    Formatting

--- LowPy 2021-01-26 01:16:41/plot.py
+++ LowPy 2021-01-26 01:16:41/plot.py
@@ -5,15 +5,13 @@ import matplotlib.pyplot as plt
 import matplotlib.axes as ax
 
 #matplotlib.rcParams['backend'] = "WXAgg" #"Qt4Agg"
-plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.family']     = 'sans-serif'
 plt.rcParams['font.sans-serif'] = ['Times New Roman'] + plt.rcParams['font.sans-serif']
-plt.rcParams['axes.linewidth'] = 2
-plt.rcParams['axes.edgecolor'] = 'grey'
+plt.rcParams['axes.linewidth']  = 2
+plt.rcParams['axes.edgecolor']  = 'grey'
 
 fileLocation = os.path.dirname(os.path.realpath(__file__))
 data = pd.read_csv(fileLocation+"/Test/Accuracy.csv",index_col=0)
-
-
 
 ax = data.plot(linewidth = 3,figsize=(5,3),cmap='coolwarm')
 plt.legend(title=" σ ",loc="center left", bbox_to_anchor=(1, 0.5))
