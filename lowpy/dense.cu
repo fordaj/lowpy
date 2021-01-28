8d77750837b6683fe5554f819c18e60acbc71002
fordaj <26288298+fordaj@users.noreply.github.com>
2021-01-28 05:30:06 -0500

    Change "L" to "V" Fixes #41

--- lowpy/dense.cu
+++ lowpy/dense.cu
@@ -9,7 +9,7 @@ __global__ void propagate(
             double  * __restrict__  y,
             double  * __restrict__  z
 ){
-    int l = blockIdx.x;
+    int v = blockIdx.x;
     int j = blockIdx.y;
     int J = gridDim.y;
     double sum = 0;
@@ -17,9 +17,9 @@ __global__ void propagate(
     if (iteration > -1){
         inputIdx = iteration * I;
     }
-    for (int i = 0; i < I; i++) sum += x[inputIdx+i]*w[l*J*I+j*I+i];
-    y[l*J+j] = sum + b[l*J+j];
-    z[l*J+j] = 1/(1 + exp(-y[l*J+j]));
+    for (int i = 0; i < I; i++) sum += x[inputIdx+i]*w[v*J*I+j*I+i];
+    y[v*J+j] = sum + b[v*J+j];
+    z[v*J+j] = 1/(1 + exp(-y[v*J+j]));
 }
 __global__ void backpropagate(
     const   int                     iteration,
@@ -40,7 +40,7 @@ __global__ void backpropagate(
             double * __restrict__   mb,
             double * __restrict__   mw
 ){
-    int l   = blockIdx.x;
+    int v   = blockIdx.x;
     int j   = blockIdx.y;
     int J   = gridDim.y;
     int I_n = gridDim.y;
@@ -50,27 +50,27 @@ __global__ void backpropagate(
     }
     if (label > -1){
         if (j == label){
-            dedz[l*J+j] = z[l*J+j] - 1;
+            dedz[v*J+j] = z[v*J+j] - 1;
         }else{
-            dedz[l*J+j] = z[l*J+j] - 0;
+            dedz[v*J+j] = z[v*J+j] - 0;
         }
     }else{
         double sum = 0;
-        for (int j_n = 0; j_n < J_n; j_n++) sum += w_n[l*J_n*I_n+j+j_n*I_n] * dedz_n[l*J_n+j_n] * dzdy_n[l*J_n+j_n];
-        dedz[l*J+j] = sum;
+        for (int j_n = 0; j_n < J_n; j_n++) sum += w_n[v*J_n*I_n+j+j_n*I_n] * dedz_n[v*J_n+j_n] * dzdy_n[v*J_n+j_n];
+        dedz[v*J+j] = sum;
     }
-    dzdy[l*J+j] = z[l*J+j] * (1 - z[l*J+j]);
+    dzdy[v*J+j] = z[v*J+j] * (1 - z[v*J+j]);
     // b[l*J+j]   -= (beta[l] * mb[l*J+j] + alpha[l] * dedz[l*J+j] * dzdy[l*J+j]);
     // mb[l*J+j]  = (beta[l] * mb[l*J+j] + alpha[l] * dedz[l*J+j] * dzdy[l*J+j]);
-    mb[l*J+j]  = beta[l] * mb[l*J+j] - alpha[l] * dedz[l*J+j] * dzdy[l*J+j];
-    b[l*J+j]   += mb[l*J+j];
+    mb[v*J+j]  = beta[v] * mb[v*J+j] - alpha[v] * dedz[v*J+j] * dzdy[v*J+j];
+    b[v*J+j]   += mb[v*J+j];
     
 
     for (int i = 0; i < I; i++){
         //w[l*J*I+j*I+i]     -= (beta[l] * mw[l*J*I+j*I+i] + alpha[l] * dedz[l*J+j] * dzdy[l*J+j] * x[inputIdx+i]);
         //mw[l*J*I+j*I+i]     = (beta[l] * mw[l*J*I+j*I+i] + alpha[l] * dedz[l*J+j] * dzdy[l*J+j] * x[inputIdx+i]);
-        mw[l*J*I+j*I+i]   = beta[l] * mw[l*J*I+j*I+i] - alpha[l] * dedz[l*J+j] * dzdy[l*J+j] * x[inputIdx+i];
-        w[l*J*I+j*I+i]    += mw[l*J*I+j*I+i];
+        mw[v*J*I+j*I+i]   = beta[v] * mw[v*J*I+j*I+i] - alpha[v] * dedz[v*J+j] * dzdy[v*J+j] * x[inputIdx+i];
+        w[v*J*I+j*I+i]    += mw[v*J*I+j*I+i];
         
     }
 }
@@ -79,20 +79,20 @@ __global__ void argmax(
         const   double  * __restrict__  z,
                 int     * __restrict__  hits
 ){
-    int l = blockIdx.x;
+    int v = blockIdx.x;
     int j = blockIdx.y;
     int J = gridDim.y;
     if (j == 0){
         double maxVal = 0;
         int maxIdx = 0;
         for (int i = 0; i < J; i++){
-            if (z[l*J+i] > maxVal){
+            if (z[v*J+i] > maxVal){
                 maxIdx = i;
-                maxVal = z[l*J+i];
+                maxVal = z[v*J+i];
             }
         }
         if (maxIdx == label){
-            hits[l] += 1;
+            hits[v] += 1;
         }
     }
 }
