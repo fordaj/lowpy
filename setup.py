795b779cb024bfeb3f4c2839dbb7fb337601d291
fordaj <26288298+fordaj@users.noreply.github.com>
2021-01-30 19:02:21 -0500

    Incremented to v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lowpy",
-    version="0.1.4",
+    version="0.1.5",
     author="Andrew Ford",
     author_email="author@example.com",
     description="High level GPU simulations of low level device characteristics in ML algorithms",
